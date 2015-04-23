54ce00bf180a33978515fecd86cf5f5b781bbef6
NiShuang <klqbtnsns@hotmail.com>
2015-04-23 15:05:00 +0800

    k

--- src/main/java/ui/main/Main.java
+++ src/main/java/ui/main/Main.java
@@ -4,8 +4,9 @@ import rmi.Server;
 import ui.main.Frame;
 
 public class Main {
+	public static Frame frame = null;
 	public static void main(String[] args) {
 		new Server();
-		new Frame();
+		frame = new Frame();
 	}
 }
