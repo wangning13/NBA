75433baec481dd78c5f3d1fb13fbe0945b2bed85
zzy131250 <zzy131250@gmail.com>
2015-05-04 16:19:56 +0800

    5.4

--- src/main/java/console/Console.java
+++ src/main/java/console/Console.java
@@ -2,15 +2,52 @@ package console;
 
 import java.io.PrintStream;
 
+import de.tototec.cmdoption.CmdCommand;
+import de.tototec.cmdoption.CmdOption;
+import de.tototec.cmdoption.CmdlineParser;
+
 public class Console {
+	
+	PrintStream out;
 
+	@CmdCommand(names = {"-player"},description = "Show player data")
+	public class PlayerModel {
+		
+		@CmdOption(names = {"-season"},description = "Show season player king")
+		public void setSeasonPlayerKing() {
+			out.println("dd");
+		}
+		
+	}
+	
+	@CmdCommand(names = {"-team"},description = "Show team data")
+	public class TeamModel {
+		
+		
+		
+	}
+	
 	public void execute(PrintStream out, String[] args) {
+		this.out = out;
+		CmdlineParser cp;
+		switch (args[0]) {
+		case "-player":
+			PlayerModel p = new PlayerModel();
+			cp = new CmdlineParser(p);
+			cp.parse(args);
+			break;
 
+		case "-team":
+			TeamModel t = new TeamModel();
+			cp = new CmdlineParser(t);
+			cp.parse(args);
+			break;
+		}
 	}
 	
 	public static void main(String[] args){
 		Console console = new Console();
-		console.execute(System.out, args);
+		console.execute(System.out, new String[]{"-player","-season"});
 	}
 	
 }
