aa953f55a5cdfc7e091fd0a38a36a15e6776af68
zzy131250 <zzy131250@gmail.com>
2015-05-04 15:38:44 +0800

    5.4

--- src/main/java/console/Console.java
+++ src/main/java/console/Console.java
@@ -5,11 +5,19 @@ import java.io.PrintStream;
 public class Console {
 
 	public void execute(PrintStream out, String[] args) {
-
+		switch (args[0]) {
+		case "--datasource":
+			
+			break;
+		
+		case "-player":
+			new PlayerModel();
+			break;
+		
+		case "-team":
+			
+			break;
+		}
 	}
 	
-	public static void main(String[] args){
-		Console console = new Console();
-		console.execute(System.out, args);
-	}
 }
