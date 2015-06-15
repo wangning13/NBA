13379e5455ca721805c270c8c55fda7288d96d33
wangning13 <1287983597@qq.com>
2015-06-15 21:36:38 +0800

    new

--- src/main/java/businesslogic/teambl/TeamRank.java
+++ src/main/java/businesslogic/teambl/TeamRank.java
@@ -232,7 +232,167 @@ public class TeamRank implements TeamRankService{
 			}
     	return teamVOs;
     }
-    //多元线性回归方程，数组表示 a1,a2,a3,...am,a0,大小m+1
+    //通过多元线性回归方程预测球队得分
+    public double[] TwoScoring(String TeamName1,String TeamName2) {
+		double[] Scoring = new double[2];
+		GetTeamdataDataService g = new GetTeamdata();
+		ArrayList<TeamVO> teamVOs1 = this.getTeamData("14-15","`east/west`='E'", "wins", "DESC");
+		ArrayList<TeamVO> teamVOs2 = this.getTeamData("14-15","`east/west`='W'", "wins", "DESC");
+		ArrayList<TeamVO> teamVOs = new ArrayList<>();
+		double[] team1 = new double[15];
+		double[] team2 = new double[15];
+		for (int i = 0; i < teamVOs1.size(); i++) {
+			teamVOs.add(teamVOs1.get(i));
+		}
+		for (int i = 0; i < teamVOs2.size(); i++) {
+			teamVOs.add(teamVOs2.get(i));
+		}
+		for (int i = 0; i < teamVOs.size(); i++) {
+			if (TeamName1.equals(teamVOs.get(i).getTeamName())) {
+				team1[0] = 1;
+				team1[1] = teamVOs.get(i).getAverageBackboard();
+				team1[2] = teamVOs.get(i).getAverageTurnOver();
+				team1[3] = teamVOs.get(i).getAverageFoul();
+				team1[4] = teamVOs.get(i).getAverageAsist();
+				team1[5] = teamVOs.get(i).getAverageSteal();
+				team1[6] = teamVOs.get(i).getAverageBlock();
+				team1[7] = teamVOs.get(i).getFieldGoalPercentage();
+				team1[8] = teamVOs.get(i).getThreePointShotPercentage();
+				team1[9] = teamVOs.get(i).getFreeThrowPercentage();
+				team1[10] = teamVOs.get(i).getAverageOffensiveRebound()+teamVOs.get(i).getAverageDefensiveRebound();
+				team1[11] = (double)teamVOs.get(i).getOpponentFieldGoal()/teamVOs.get(i).getMatches();
+				team1[12] = (double)teamVOs.get(i).getOpponentFieldGoalAttempts()/teamVOs.get(i).getMatches();
+				team1[13] = (double)teamVOs.get(i).getOpponentTurnOver()/teamVOs.get(i).getMatches();
+				team1[14] = (double)teamVOs.get(i).getFreeThrowAttempts()/teamVOs.get(i).getMatches();
+			}else if (TeamName2.equals(teamVOs.get(i).getTeamName())) {
+				team2[0] = 1;
+				team2[1] = teamVOs.get(i).getAverageBackboard();
+				team2[2] = teamVOs.get(i).getAverageTurnOver();
+				team2[3] = teamVOs.get(i).getAverageFoul();
+				team2[4] = teamVOs.get(i).getAverageAsist();
+				team2[5] = teamVOs.get(i).getAverageSteal();
+				team2[6] = teamVOs.get(i).getAverageBlock();
+				team2[7] = teamVOs.get(i).getFieldGoalPercentage();
+				team2[8] = teamVOs.get(i).getThreePointShotPercentage();
+				team2[9] = teamVOs.get(i).getFreeThrowPercentage();
+				team2[10] = teamVOs.get(i).getAverageOffensiveRebound()+teamVOs.get(i).getAverageDefensiveRebound();
+				team2[11] = (double)teamVOs.get(i).getOpponentFieldGoal()/teamVOs.get(i).getMatches();
+				team2[12] = (double)teamVOs.get(i).getOpponentFieldGoalAttempts()/teamVOs.get(i).getMatches();
+				team2[13] = (double)teamVOs.get(i).getOpponentTurnOver()/teamVOs.get(i).getMatches();
+				team2[14] = (double)teamVOs.get(i).getFreeThrowAttempts()/teamVOs.get(i).getMatches();
+			}
+			
+		}
+		double[] a = this.LinerRegression2("14-15");
+		double Scoring1 = 0;
+		double Scoring2 = 0;
+		for (int j = 0; j < team1.length; j++) {
+			Scoring1 = Scoring1 + team1[j]*a[j];
+		}
+		Scoring1 = Scoring1 + a[a.length-1];
+		for (int j = 0; j < team1.length; j++) {
+			Scoring2 = Scoring2 + team2[j]*a[j];
+		}
+		Scoring2 = Scoring2 + a[a.length-1];
+		Scoring[0] = Scoring1;
+		Scoring[1] = Scoring2;
+		return Scoring;
+	}
+    //多元线性回归方程，数组表示 a1,a2,a3,...am,a0,大小m+1，y表示自己得分
+    public double[] LinerRegression2(String season) {
+    	GetTeamdataDataService g = new GetTeamdata();
+		ArrayList<ArrayList<TeamPO>> r = g.getHostGuestdata(season);
+		ArrayList<TeamPO> host = r.get(0);
+		ArrayList<TeamPO> guest = r.get(1);
+		
+		int m = 15;
+		int n = host.size()+guest.size();
+		double[][] x = new double[m][n];
+		double[] y = new double[n];
+		for (int i = 0; i < host.size(); i++) {
+			//主客场
+			x[0][i]= 1; 
+			//篮板数
+			x[1][i]= host.get(i).getBackboard();
+			//失误数
+			x[2][i]= host.get(i).getTurnOver();
+			//犯规数
+			x[3][i]= host.get(i).getFoul();
+			//助攻数
+			x[4][i]= host.get(i).getAssist();
+			//抢断数
+			x[5][i]= host.get(i).getSteal();
+			//盖帽数
+			x[6][i]= host.get(i).getBlock();
+			//投篮命中率
+			double fieldGoalPercentage = ((double)host.get(i).getFieldGoal())/host.get(i).getFieldGoalAttempts();
+			x[7][i]= fieldGoalPercentage; 
+			//三分命中率
+			double threePointShotPercentage = ((double)host.get(i).getThreePointFieldGoal())/host.get(i).getThreePointFieldGoalAttempts();
+			x[8][i]= threePointShotPercentage; 
+			//罚球命中率
+			double freeThrowPercentage = ((double)host.get(i).getFreeThrow())/host.get(i).getFreeThrowAttempts();
+			x[9][i] = freeThrowPercentage;
+			//对手篮板数
+			x[10][i]= host.get(i).getOpponentDefensiveRebound()+host.get(i).getOpponentOffensiveRebound();
+			//对手投篮命中数
+			double opponentFieldGoal = host.get(i).getOpponentFieldGoal();
+			x[11][i]= opponentFieldGoal; 
+			//对手投篮出手次数
+			double opponentFieldGoalAttempts = host.get(i).getOpponentFieldGoalAttempts();
+			x[12][i]= opponentFieldGoalAttempts; 
+			//对手失误数
+			x[13][i]= host.get(i).getOpponentTurnOver();
+			//对手罚球数
+			double opponentFreeThrowAttempts = host.get(i).getOpponentFreeThrowAttempts();
+			x[14][i]= opponentFreeThrowAttempts;
+			//自己得分-对手得分
+			y[i]= host.get(i).getScoring();
+		}
+		for (int i = host.size(); i < n; i++) {
+			//主客场
+			x[0][i]= -1; 
+			//篮板数
+			x[1][i]= guest.get(i-host.size()).getBackboard();
+			//失误数
+			x[2][i]= guest.get(i-host.size()).getTurnOver();
+			//犯规数
+			x[3][i]= guest.get(i-host.size()).getFoul();
+			//助攻数
+			x[4][i]= guest.get(i-host.size()).getAssist();
+			//抢断数
+			x[5][i]= guest.get(i-host.size()).getSteal();
+			//盖帽数
+			x[6][i]= guest.get(i-host.size()).getBlock();
+			//投篮命中率
+			double fieldGoalPercentage = ((double)guest.get(i-host.size()).getFieldGoal())/guest.get(i-host.size()).getFieldGoalAttempts();
+			x[7][i]= fieldGoalPercentage; 
+			//三分命中率
+			double threePointShotPercentage = ((double)guest.get(i-host.size()).getThreePointFieldGoal())/guest.get(i-host.size()).getThreePointFieldGoalAttempts();
+			x[8][i]= threePointShotPercentage; 
+			//罚球命中率
+			double freeThrowPercentage = ((double)guest.get(i-host.size()).getFreeThrow())/guest.get(i-host.size()).getFreeThrowAttempts();
+			x[9][i] = freeThrowPercentage;
+			//对手篮板数
+			x[10][i]= guest.get(i-host.size()).getOpponentDefensiveRebound()+guest.get(i-host.size()).getOpponentOffensiveRebound();
+			//对手投篮命中数
+			double opponentFieldGoal = guest.get(i-host.size()).getOpponentFieldGoal();
+			x[11][i]= opponentFieldGoal; 
+			//对手投篮出手次数
+			double opponentFieldGoalAttempts = guest.get(i-host.size()).getOpponentFieldGoalAttempts();
+			x[12][i]= opponentFieldGoalAttempts; 
+			//对手失误数
+			x[13][i]= guest.get(i-host.size()).getOpponentTurnOver();
+			//对手罚球数
+			double opponentFreeThrowAttempts = guest.get(i-host.size()).getOpponentFreeThrowAttempts();
+			x[14][i]= opponentFreeThrowAttempts;
+			//自己得分-对手得分
+			y[i]= guest.get(i-host.size()).getScoring(); 
+		}
+		double[] a = sqt2(x,y,m,n);
+		return a;
+	}
+    //多元线性回归方程，数组表示 a1,a2,a3,...am,a0,大小m+1，y表示自己得分与对手得分差值
     public double[] LinerRegression(String season) {
     	GetTeamdataDataService g = new GetTeamdata();
 		ArrayList<ArrayList<TeamPO>> r = g.getHostGuestdata(season);
@@ -699,7 +859,7 @@ public class TeamRank implements TeamRankService{
 	
 	//研究主客场对球队最终得分的影响结果是否显著
 	public double[] HostOrGuest(String season) {
-		double[] a = new double[5];
+		double[] a = new double[11];
 		GetTeamdataDataService g = new GetTeamdata();
 		ArrayList<ArrayList<TeamPO>> teampoList = g.getHostGuestdata(season);
 		ArrayList<TeamPO> host = teampoList.get(0);
@@ -742,14 +902,25 @@ public class TeamRank implements TeamRankService{
 		for (int i = 0; i < r; i++) {
 			Se = Se + (y[i]-y_average)*(y[i]-y_average);
 		}
+		a[0] = Sa;
+		a[1] = Se;
+		a[2] = St;
 		int ft = 2*r-1;//St的自由度
 		int fa = 1;//Sa的自由度
 		int fe = 2*r-2;//Se的自由度
+		a[3] = fa;
+		a[4] = fe;
+		a[5] = ft;
 		double Va = Sa/fa;
 		double Ve = Se/fe;
+		a[6] = Va;
+		a[7] = Ve;
 		double F = Va/Ve;
-		
-		
+		a[8] = F;
+		double F1 = 2.46261492591;
+		double F2 = 3.5126840636;
+		a[9] = F1;
+		a[10] = F2;
 		
 		return a;
 	}
